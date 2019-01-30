8c9f2e51b250703016111ec91a97be75dab44e2c
amelia sayle <ameliasayle@gmail.com>
2019-01-30 12:30:44 -0700

    fds

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+new Vue({
+	el: '#app',
+	data: {
+		isRunning: false,
+		playerHealth: 100,
+		monsterHealth: 100
+	},
+	methods: {
+		start: function() {
+			this.isRunning = true;
+		},
+		giveUp: function(){
+			this.isRunning = false;
+			this.playerHealth = 100;
+			this.monsterHealth = 100;
+		},
+		attack: function(){
+			this.playerHealth -= Math.floor(Math.random() * 18) + 6;
+			this.monsterHealth -= Math.floor(Math.random() * 10) + 3;
+		},
+		special: function (){
+			this.playerHealth -= Math.floor(Math.random() * 18) + 6;
+			this.monsterHealth -= Math.floor(Math.random() * 13) + 5;
+		},
+		heal: function(){
+			this.playerHealth += Math.floor(Math.random() * 16) + 10;
+			this.playerHealth -= Math.floor(Math.random() * 18) + 6;
+		}
+	}
+})
+
+
